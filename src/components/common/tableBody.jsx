bf7d63a1161d5e6a73a206661b6c126fba8679bd
Sasho Tatarski <sashotatarski@gmail.com>
2022-09-19 19:22:43 +0300

    Unique keys fixed

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -4,17 +4,21 @@ import React from 'react';
 const TableBody = ({ data, columns }) => {
   const renderCell = (item, column) => {
     if (column.content) return column.content(item);
-    const result = _.get(item, column.path);
-    console.log(result);
-    return result;
+    return _.get(item, column.path);
+  };
+
+  const createKey = (item, column) => {
+    return item._id + (column.path || column.key);
   };
 
   return (
     <tbody>
       {data.map((item) => (
-        <tr>
+        <tr key={item._id}>
           {columns.map((column) => (
-            <td>{renderCell(item, column)}</td>
+            <td key={createKey(item, column)}>
+              {renderCell(item, column)}
+            </td>
           ))}
         </tr>
       ))}
