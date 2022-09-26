2901c7f1ce054725e819c09adc66e57ed9ab7dd3
Sasho Tatarski <sashotatarski@gmail.com>
2022-09-26 14:44:01 +0300

    Registerform component added

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,45 +1,35 @@
-import React from "react";
-import Joi from "joi-browser";
-import Form from "./common/form";
+import Joi from 'joi-browser';
+import React from 'react';
+import { useForm } from './common/useForm';
 
-class RegisterForm extends Form {
-  state = {
-    data: { username: "", password: "", name: "" },
-    errors: {}
+const RegisterForm = () => {
+  const schema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().min(5).label('Password'),
+    name: Joi.string().required().label('Name'),
   };
 
-  schema = {
-    username: Joi.string()
-      .required()
-      .email()
-      .label("Username"),
-    password: Joi.string()
-      .required()
-      .min(5)
-      .label("Password"),
-    name: Joi.string()
-      .required()
-      .label("Name")
-  };
-
-  doSubmit = () => {
+  const doSubmit = () => {
     // Call the server
     console.log('Submitted');
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Register</h1>
-        <form onSubmit={this.handleSubmit}>
-          {this.renderInput("username", "Username")}
-          {this.renderInput("password", "Password", "password")}
-          {this.renderInput("name", "Name")}
-          {this.renderButton("Register")}
-        </form>
-      </div>
-    );
-  }
-}
+  const { handleSubmit, renderButton, renderInput } = useForm(
+    schema,
+    doSubmit
+  );
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        {renderInput('username', 'Username')}
+        {renderInput('password', 'Password', 'password')}
+        {renderInput('name', 'Name')}
+        {renderButton('Register')}
+      </form>
+    </div>
+  );
+};
 
 export default RegisterForm;
